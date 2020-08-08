aee71522813f42fe2130162cef536539c93b591a
VergisT <vergist@gmail.com>
2020-08-08 20:55:32 +0200

    firstvvcx

---
+++ products/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+
+def index(request):
+    # return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+
+def new(request):
+    return HttpResponse('new products World')
